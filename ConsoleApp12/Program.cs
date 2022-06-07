2706551a37f9941e5b2cc0de04c022c1fc95723a
saydi <saydi@ENGINEER>
2022-06-07 18:32:06 +0700

    Добавьте файлы проекта.

---
+++ ConsoleApp12/Program.cs
@@ -0,0 +1,21 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Введите x ");
+        int x = int.Parse(Console.ReadLine());
+        int a = 1000;
+        int kol = 0;
+        while (a != 0)
+        {
+            Console.WriteLine("Введите число: ");
+            int y = int.Parse(Console.ReadLine());
+            a = y;
+            if (y > x)
+            {
+                kol += y;
+            }
+        }
+        Console.WriteLine("Сумма чисел, больших х = " + kol);
+    }
+}
